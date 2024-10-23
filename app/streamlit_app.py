8f13e241c56cd1cd39e30df1aec6fd140c2588e3
Kamesh Sampath <kamesh.sampath@hotmail.com>
2024-10-23 16:11:44 +0530

    (feat!): add streamlit app

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -14,11 +14,11 @@ st.divider()
 st.markdown(
     """A simple TODO application to demonstrate how we can use Streamlit to build a CRUD application. This application gives a quick overview of Streamlit features,
 
-* Using DataFrame to show list of TODO
+* A graphical representation of the TODOS grouped by Category, Status
 
-* Use Form to perform the updates to TODO
+* Using DataFrame to show list of TODO 
 
-The script to create Database and its data is available at https://github.com/kameshsampath/sf-git-integration-demo.git.
+The script to create Database and its data is available at https://github.com/Snowflake-Labs/snow-cli-demo.git.
 """
 )
 st.divider()
@@ -28,31 +28,34 @@ session = get_active_session()
 db_name = "TODO_APP_DB"
 schema = "DATA"
 table = "TODOS"
+
 table_fqn = f"{db_name}.{schema}.{table}"
-categories = {
-    "learning": "Learning",
-    "code": "Code",
-    "personal": "Personal",
-    "work": "Work",
-    "finance": "Finance",
-    "government": "Government",
-    "others": "Others",
-    "family": "Personal",
-}
+todo_df = session.table(table_fqn)
+todo_category_count_df = (
+    todo_df.select(["category", "status"]).groupBy(["category", "status"]).count()
+)
 
+# Execute the query and convert it into a Pandas dataframe
+queried_data = todo_category_count_df.to_pandas()
+# set all True to "Done" and all No "WIP"
+queried_data.loc[queried_data["STATUS"] == True, "STATUS"] = "Done"
+queried_data.loc[queried_data["STATUS"] == False, "STATUS"] = "WIP"
 
-@st.cache_data(show_spinner=True)
-def get_todos():
-    todos = session.table(table_fqn)
-    return todos.to_pandas()
+# Create a simple bar chart
+# See docs.streamlit.io for more types of charts
+st.subheader("Tasks by category")
+st.bar_chart(
+    data=queried_data,
+    x="CATEGORY",
+    y="COUNT",
+    color="STATUS",
+)
 
-
-st.session_state.todos_df = get_todos()
-st.session_state.edit = False
-
+df2 = todo_df.select(["title", "category", "status"])
+st.subheader("Underlying data")
 ## List to show all todos
 event = st.dataframe(
-    st.session_state.todos_df,
+    df2,
     hide_index=True,
     selection_mode=["single-row"],
     on_select="rerun",
@@ -62,11 +65,6 @@ event = st.dataframe(
             label="Title",
             help="Title of the Task",
             width="medium",
-        ),
-        "DESCRIPTION": st.column_config.TextColumn(
-            label="Desc",
-            width="medium",
-            help="Description of the Task",
         ),
         "CATEGORY": st.column_config.TextColumn(
             label="Category",
@@ -79,85 +77,4 @@ event = st.dataframe(
             width="small",
         ),
     },
-    use_container_width=True,
 )
-
-st.markdown(
-    """
-            ## Add/Edit Todo
-            Use the following form to edit and update the TODO.
-"""
-)
-
-
-if event.selection and event.selection.rows:
-    st.write(event.selection)
-    if event.selection.rows[0] is not None:
-        st.session_state.edit = True
-        st.session_state.todo_title = st.session_state.todos_df.iloc[
-            event.selection.rows[0]
-        ]["TITLE"]
-        st.session_state.todo_desc = st.session_state.todos_df.iloc[
-            event.selection.rows[0]
-        ]["DESCRIPTION"]
-        st.session_state.todo_category = st.session_state.todos_df.iloc[
-            event.selection.rows[0]
-        ]["CATEGORY"]
-        st.session_state.todo_status = st.session_state.todos_df.iloc[
-            event.selection.rows[0]
-        ]["STATUS"]
-
-
-with st.container():
-    if st.session_state.edit:
-        st.markdown("### Edit Todo")
-    else:
-        st.markdown("### Add Todo")
-    with st.container():
-        title = st.text_input(
-            "Title",
-            max_chars=30,
-            key="todo_title",
-        )
-        desc = st.text_area(
-            "Description",
-            max_chars=280,
-            key="todo_desc",
-        )
-
-        ccol1, _ = st.columns(2)
-        with ccol1:
-            category = st.selectbox(
-                "Pick a category",
-                sorted(categories),
-                key="todo_category",
-                format_func=lambda x: categories[x],
-            )
-        scol1, _ = st.columns(2)
-        with scol1:
-            status = st.checkbox(
-                "Status",
-                key="todo_status",
-            )
-
-    _, _, col2, col3, _, _ = st.columns(6)
-    with col3:
-        submitted = None
-        if st.session_state.edit:
-            st.button("Update", type="primary")
-        else:
-            st.button("Add", type="primary")
-        if submitted:
-            logger.debug("row update")
-            # TODO add record to DF
-    with col2:
-        reset_button = st.button(
-            "Clear",
-            key="todo_reset",
-        )
-        if reset_button:
-            st.session_state.todo_title = None
-            st.session_state.todo_desc = None
-            st.session_state.todo_category = None
-            st.session_state.todo_status = False
-            st.session_state.edit = False
